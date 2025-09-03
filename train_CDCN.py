a7dc2f713fc19b3035be50753a7710a2e25f77fd
quangdm <quangdm@vega.com.vn>
2025-09-03 17:59:09 +0700

    update train CDCN

--- train_CDCN.py
+++ train_CDCN.py
@@ -32,7 +32,7 @@ def contrast_depth_conv(input):
     
     kernel_filter = np.array(kernel_filter_list, np.float32)
     
-    kernel_filter = torch.from_numpy(kernel_filter.astype(np.float)).float().cuda()
+    kernel_filter = torch.from_numpy(kernel_filter.astype(np.float32)).float().cuda()
     # weights (in_channel, out_channel, kernel, kernel)
     kernel_filter = kernel_filter.unsqueeze(dim=1)
     
@@ -103,135 +103,122 @@ def validate_batch_images(model, dataloader_val, epoch, log_dir):
 
 
 # main function
-def train_test(dir_root, file_train_csv_path, file_val_csv_path):
-    # GPU  & log file  -->   if use DataParallel, please comment this command
-    #os.environ["CUDA_VISIBLE_DEVICES"] = "%d" % (args.gpu)
+def train_test(dir_root, file_train_csv_path, file_val_csv_path, args):
 
     isExists = os.path.exists(args.log)
     if not isExists:
         os.makedirs(args.log)
         
-    with open(args.log+'/'+ args.log+'_log.txt', 'w') as log_file:
-        echo_batches = args.echo_batches
+    echo_batches = args.echo_batches
 
-        print("==============START==============")
+    print("==============START==============")
 
-        log_file.write('"==============START=============="\n')
-        log_file.flush()
 
-        # load the network, load the pre-trained model in UCF101?
-        finetune = args.finetune
-        if finetune==True:
-            print('finetune!\n')
+    # load the network, load the pre-trained model in UCF101?
+    finetune = args.finetune
+    if finetune==True:
+        print('finetune!\n')
 
-        else:
-            print('train from scratch!\n')
-            log_file.write('train from scratch!\n')
-            log_file.flush()
-            
-            #model = CDCNpp( basic_conv=Conv2d_cd, theta=0.7)
-            model = CDCNpp( basic_conv=Conv2d_cd, theta=args.theta)
-            model = model.cuda()
+    else:
 
-            lr = args.lr
-            optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.00005)
-            scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+        model = CDCNpp( basic_conv=Conv2d_cd, theta=args.theta)
+        model = model.cuda()
+
+        lr = args.lr
+        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.00005)
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+    
+    criterion_absolute_loss = nn.MSELoss().cuda()
+    criterion_contrastive_loss = Contrast_depth_loss().cuda() 
+    
+    ACER_save = 1.0
+    
+    for epoch in range(args.epochs):  # loop over the dataset multiple times
+        scheduler.step()
+        if (epoch + 1) % args.step_size == 0:
+            lr *= args.gamma
+
         
-        print(model) 
+        loss_absolute = AvgrageMeter()
+        loss_contra =  AvgrageMeter()
+        #top5 = utils.AvgrageMeter()
         
+        model.train()
         
-        criterion_absolute_loss = nn.MSELoss().cuda()
-        criterion_contrastive_loss = Contrast_depth_loss().cuda() 
+        # load train data
+        train_data = Spoofing_Train_Images_Custom(
+            csv_path=file_train_csv_path, 
+            root_dir=dir_root,
+            transform=transforms.Compose([RandomErasing(), RandomHorizontalFlip(),  ToTensor(), Cutout(), Normaliztion()])
+            )
         
-        ACER_save = 1.0
+        dataloader_train = DataLoader(train_data, batch_size=args.batchsize, shuffle=True, num_workers=4)
         
-        for epoch in range(args.epochs):  # loop over the dataset multiple times
-            scheduler.step()
-            if (epoch + 1) % args.step_size == 0:
-                lr *= args.gamma
-
-            
-            loss_absolute = AvgrageMeter()
-            loss_contra =  AvgrageMeter()
-            #top5 = utils.AvgrageMeter()
-            
-            model.train()
-            
-            # load train data
-            train_data = Spoofing_Train_Images_Custom(
-                csv_path=file_train_csv_path, 
-                root_dir=dir_root,
-                transform=transforms.Compose([RandomErasing(), RandomHorizontalFlip(),  ToTensor(), Cutout(), Normaliztion()])
+        # load val data
+        val_data = Spoofing_Val_Images_Custom(
+                    csv_path=file_val_csv_path, 
+                    root_dir=dir_root, 
+                    transform=transforms.Compose([Normaliztion_valtest(), ToTensor_valtest()])
                 )
-            
-            dataloader_train = DataLoader(train_data, batch_size=args.batchsize, shuffle=True, num_workers=4)
-            
-            # load val data
-            val_data = Spoofing_Val_Images_Custom(
-                        csv_path=file_val_csv_path, 
-                        root_dir=dir_root, 
-                        transform=transforms.Compose([Normaliztion_valtest(), ToTensor_valtest()])
-                    )
-                    
-            dataloader_val = DataLoader(val_data, batch_size=args.batchsize, shuffle=False, num_workers=4)
-            
-            for i, sample_batched in enumerate(dataloader_train):
-                # get the inputs
-                inputs, binary_mask, spoof_label = sample_batched['image_x'].cuda(), sample_batched['binary_mask'].cuda(), sample_batched['spoofing_label'].cuda() 
+                
+        dataloader_val = DataLoader(val_data, batch_size=args.batchsize, shuffle=False, num_workers=4)
+        
+        for i, sample_batched in enumerate(dataloader_train):
+            # get the inputs
+            inputs, binary_mask, spoof_label = sample_batched['image_x'].cuda(), sample_batched['binary_mask'].cuda(), sample_batched['spoofing_label'].cuda() 
 
-                optimizer.zero_grad()
+            optimizer.zero_grad()
 
-                # forward + backward + optimize
-                map_x, embedding, x_Block1, x_Block2, x_Block3, x_input =  model(inputs)
-                
-                #pdb.set_trace()
-                #pdb.set_trace()
-                absolute_loss = criterion_absolute_loss(map_x, binary_mask)
-                contrastive_loss = criterion_contrastive_loss(map_x, binary_mask)
-                
-                loss =  absolute_loss + contrastive_loss
-                
-                loss.backward()
-                
-                optimizer.step()
-                
-                n = inputs.size(0)
-                loss_absolute.update(absolute_loss.data, n)
-                loss_contra.update(contrastive_loss.data, n)
-                
-                
-                if i % echo_batches == echo_batches - 1:    # print every 50 mini-batches
-                    
-                    # visualization
-                    #FeatureMap2Heatmap(x_input, x_Block1, x_Block2, x_Block3, map_x)
-
-                    # log written
-                    print('epoch:%d, mini-batch:%3d, lr=%f, Absolute_Depth_loss= %.4f, Contrastive_Depth_loss= %.4f' % (epoch + 1, i + 1, lr,  loss_absolute.avg, loss_contra.avg))
+            # forward + backward + optimize
+            map_x, embedding, x_Block1, x_Block2, x_Block3, x_input =  model(inputs)
             
-                #break            
-                
-            # whole epoch average
-            print('epoch:%d, Train:  Absolute_Depth_loss= %.4f, Contrastive_Depth_loss= %.4f\n' % (epoch + 1, loss_absolute.avg, loss_contra.avg))
-            log_file.write('epoch:%d, Train: Absolute_Depth_loss= %.4f, Contrastive_Depth_loss= %.4f \n' % (epoch + 1, loss_absolute.avg, loss_contra.avg))
-            log_file.flush()
+            #pdb.set_trace()
+            #pdb.set_trace()
+            absolute_loss = criterion_absolute_loss(map_x, binary_mask)
+            contrastive_loss = criterion_contrastive_loss(map_x, binary_mask)
             
-            validate_batch_images(model, dataloader_val, epoch, args.log)
+            loss =  absolute_loss + contrastive_loss
+            
+            loss.backward()
+            
+            optimizer.step()
+            
+            n = inputs.size(0)
+            loss_absolute.update(absolute_loss.data, n)
+            loss_contra.update(contrastive_loss.data, n)
+            
+            
+            if i % echo_batches == echo_batches - 1:    # print every 50 mini-batches
+                
+                # visualization
+                # FeatureMap2Heatmap(x_input, x_Block1, x_Block2, x_Block3, map_x)
 
-        print('Finished Training')
+                # log written
+                print('epoch:%d, mini-batch:%3d, lr=%f, Absolute_Depth_loss= %.4f, Contrastive_Depth_loss= %.4f' % (epoch + 1, i + 1, lr,  loss_absolute.avg, loss_contra.avg))
+        
+            #break            
+            
+        # whole epoch average
+        print('epoch:%d, Train:  Absolute_Depth_loss= %.4f, Contrastive_Depth_loss= %.4f\n' % (epoch + 1, loss_absolute.avg, loss_contra.avg))
+
+        validate_batch_images(model, dataloader_val, epoch, args.log)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="save quality using landmarkpose model")
-    parser.add_argument('--gpu', type=int, default=3, help='the gpu id used for predict')
+    parser.add_argument('--gpu', type=int, default=0, help='the gpu id used for predict')
     parser.add_argument('--lr', type=float, default=0.00008, help='initial learning rate')  #default=0.0001
     parser.add_argument('--batchsize', type=int, default=9, help='initial batchsize')  #default=7  
     parser.add_argument('--step_size', type=int, default=20, help='how many epochs lr decays once')  # 500  | DPC = 400
     parser.add_argument('--gamma', type=float, default=0.5, help='gamma of optim.lr_scheduler.StepLR, decay of lr')
     parser.add_argument('--echo_batches', type=int, default=50, help='how many batches display once')  # 50
-    parser.add_argument('--epochs', type=int, default=60, help='total training epochs')
+    parser.add_argument('--epochs', type=int, default=5, help='total training epochs')
     parser.add_argument('--log', type=str, default="CDCNpp_BinaryMask_P1_07", help='log and save model name')
     parser.add_argument('--finetune', action='store_true', default=False, help='whether finetune other models')
     parser.add_argument('--theta', type=float, default=0.7, help='hyper-parameters in CDCNpp')
+    parser.add_argument('--train_csv', type=str, default="/data02/manhquang/dataset/celeba-spoof/CelebA_Spoof_/CelebA_Spoof/metas/intra_test/train_label.txt")
+    parser.add_argument('--val_csv', type=str, default="/data02/manhquang/dataset/celeba-spoof/CelebA_Spoof_/CelebA_Spoof/metas/intra_test/test_label.txt")
+    parser.add_argument('--root_dir', type=str, default="/data02/manhquang/dataset/celeba-spoof/CelebA_Spoof_/CelebA_Spoof", help='Root directory of dataset')
     args = parser.parse_args()
-    train_test()
+    train_test(dir_root=args.root_dir, file_train_csv_path=args.train_csv, file_val_csv_path=args.val_csv, args=args)
     pass
